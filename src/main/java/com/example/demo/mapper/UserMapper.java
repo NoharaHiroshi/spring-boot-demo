c7c3cf2e0fa9945e1d5d89a14306289de38ad7a3
Lands <380788433@qq.com>
2019-02-15 11:55:27 +0800

    mybatis generator 配置详解

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,6 +2,9 @@ package com.example.demo.mapper;
 
 import com.example.demo.model.User;
 
+// 与python的sqlAlchemy相比，最大的不同是sqlAlchemy是全自动的ORM，它设计了一套规范，让用户可以针对实际需要实时的、灵活的
+// 创建sql语句。
+// SpringBoot使用的mybatis是事先将可能对对象操作（增删查改）建立好，然后按需使用。
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
